2ede0acda544184536a3331d6424c41f78181840
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-03-13 23:53:35 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+// 개발용도로 쓰는 메모리에 저장하는 저장소 오직 테스트 용임
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long,Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(),member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
